11bd491aa6b3a77b759a26ca5c9d7b370650ba5b
Ivana <ivanapavlovic1303@gmail.com>
2023-04-11 14:45:16 +0200

    user pfoile changes

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,20 +1,13 @@
 <template>
-    <div class="max-w-md mx-auto bg-white shadow-md rounded p-6 mt-8">
-      <h2 class="text-2xl font-bold mb-6 text-blue-800">User Profile</h2>
+  <div class="flex flex-col lg:flex-row lg:max-w-3xl mx-auto mt-8 p-6">
+    <div class="bg-white shadow-md rounded-lg mb-6 p-6 lg:w-1/3">
+      <img class="w-32 h-32 rounded-full mx-auto mb-6" src="../../src/assets/user.jpg" alt="User avatar">
+      <h2 class="text-2xl font-bold mb-6 text-blue-800">{{ user.username }}</h2>
       <div class="mb-4">
         <label class="block text-gray-700 text-sm font-bold mb-2" for="userId">User ID</label>
         <input
           :value="user.id"
           id="userId"
-          disabled
-          class="bg-gray-100 shadow appearance-none border border-gray-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-        />
-      </div>
-      <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">Username</label>
-        <input
-          :value="user.username"
-          id="username"
           disabled
           class="bg-gray-100 shadow appearance-none border border-gray-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
         />
@@ -37,9 +30,8 @@
           class="bg-gray-100 shadow appearance-none border border-gray-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
         />
       </div>
-    
-  
     </div>
+    <div class="lg:w-2/3">
     <post-item
       v-for="post in filteredPosts"
       :key="post.id"    
@@ -47,6 +39,8 @@
       @deletePost="deletePost"
       @updatePost="updatePost"
     ></post-item>
+    </div>
+    </div>
   </template>
   
   
