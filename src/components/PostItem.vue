4495a92183d21d76845f96c7fe709bf867112ee4
Ivana <ivanapavlovic1303@gmail.com>
2023-04-11 14:39:26 +0200

    smaller post item box

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="bg-white rounded-lg border border-gray-300 shadow-md p-4 mb-4">
     <div class="flex items-center mb-2">
-      <img class="w-12 h-12 rounded-full mr-4" src="../../src/assets/user.jpg" alt="User avatar">
+      <img class="w-10 h-10 rounded-full mr-3" src="../../src/assets/user.jpg" alt="User avatar">
       <div>
-        <span class="text-gray-600 text-sm">{{ post.username }}</span>
+        <span class="text-gray-600 text-sm font-bold">{{ post.username }}</span>
       </div>
     </div>
     <div v-if="!editing">     
       <p class="text-base">{{ post.content }}</p>
-      <div class="flex items-center justify-end mt-4">
-        <button v-if="canEditOrDelete" @click="editing = true" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mr-2 focus:outline-none focus:shadow-outline">
+      <div class="flex items-center justify-end mt-2">
+        <button v-if="canEditOrDelete" @click="editing = true" class="text-gray-500 hover:text-blue-500 font-bold focus:outline-none mr-2">
           <i class="fas fa-edit"></i>
         </button>
-        <button v-if="canEditOrDelete" @click="$emit('deletePost', post.id)" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline">
+        <button v-if="canEditOrDelete" @click="$emit('deletePost', post.id)" class="text-gray-500 hover:text-red-500 font-bold focus:outline-none">
           <i class="fas fa-trash-alt"></i>
         </button>
       </div>
@@ -20,14 +20,15 @@
       <comment-list :postId="post.id"></comment-list>
     </div>
     <div v-else>
-      <textarea v-model="updatedContent" placeholder="Content" class="shadow appearance-none border border-gray-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent mb-4" rows="6"></textarea>
+      <textarea v-model="updatedContent" placeholder="What's happening?" class="shadow appearance-none border border-gray-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent mb-4" rows="4"></textarea>
       <div class="flex items-center justify-end">
-        <button @click="savePost" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mr-2 focus:outline-none focus:shadow-outline hover:shadow-md">Save</button>
-        <button @click="cancelEdit" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full mr-2 focus:outline-none focus:shadow-outline hover:shadow-md">Cancel</button>
+        <button @click="savePost" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded-full mr-2 focus:outline-none focus:shadow-outline hover:shadow-md">Save</button>
+        <button @click="cancelEdit" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-3 rounded-full mr-2 focus:outline-none focus:shadow-outline hover:shadow-md">Cancel</button>
       </div>
     </div>
   </div>
 </template>
+
 
 <script>
 import CommentList from './CommentList.vue';
