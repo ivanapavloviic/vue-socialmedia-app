797389e819c8eadec84b481e8598bce98ad63a48
Ivana <ivanapavlovic1303@gmail.com>
2023-04-11 13:59:32 +0200

    login and register styling and validation

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex justify-center items-center h-screen">
-    <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
-      <h1 class="font-bold text-2xl mb-6">Log in to your account</h1>
+  <div class="flex justify-center items-center h-screen bg-gray-100">
+    <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col w-80">
+      <h1 class="font-bold text-2xl mb-6 text-center text-gray-800">Log in to your account</h1>
       <form>
         <div class="mb-4">
           <label class="block text-gray-700 font-bold mb-2" for="username">
@@ -21,11 +21,12 @@
           </label>
           <input
             v-model="password"
-            class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
             id="password"
             type="password"
             placeholder="**********"
           >
+          <p v-if="error" class="text-red-500 text-sm">{{ error }}</p>
         </div>
         <div class="flex items-center justify-between">
           <button
@@ -35,12 +36,12 @@
           >
             Log in
           </button>
-        
         </div>
       </form>
     </div>
   </div>
 </template>
+
 
 <script>
 import axios from 'axios';
--- src/components/RegisterPage.vue
+++ src/components/RegisterPage.vue
@@ -1,30 +1,30 @@
 <template>
-  <div class="flex items-center justify-center min-h-screen bg-gray-100">
-    <div class="w-full max-w-sm">
-      <h1 class="text-3xl font-bold mb-6 text-blue-800">Register</h1>
-      <form @submit.prevent="registerUser" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div class="mb-4">
-          <label class="block font-bold mb-2" for="name">Name</label>
-          <input class="appearance-none border border-gray-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" type="text" name="name" v-model="name" required>
+  <div class="flex flex-col items-center justify-center bg-gray-100 min-h-screen">
+    <div class="bg-white w-full max-w-md p-6 rounded-lg shadow-md">
+      <h1 class="text-2xl font-bold mb-6 text-blue-500">Create your account</h1>
+      <form @submit.prevent="registerUser" class="space-y-6">
+        <div>
+          <label for="name" class="block text-gray-700 font-bold mb-2">Name</label>
+          <input v-model="name" type="text" name="name" id="name" class="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500" required>
         </div>
-        <div class="mb-4">
-          <label class="block font-bold mb-2" for="email">Email</label>
-          <input class="appearance-none border border-gray-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" type="email" name="email" v-model="email" required>
+        <div>
+          <label for="email" class="block text-gray-700 font-bold mb-2">Email</label>
+          <input v-model="email" type="email" name="email" id="email" class="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500" required>
         </div>
-        <div class="mb-4">
-          <label class="block font-bold mb-2" for="username">Username</label>
-          <input class="appearance-none border border-gray-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" type="text" name="username" v-model="username" required>
+        <div>
+          <label for="username" class="block text-gray-700 font-bold mb-2">Username</label>
+          <input v-model="username" type="text" name="username" id="username" class="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500" required>
         </div>
-        <div class="mb-4">
-          <label class="block font-bold mb-2" for="password">Password</label>
-          <input class="appearance-none border border-gray-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" type="password" name="password" v-model="password" required>
+        <div>
+          <label for="password" class="block text-gray-700 font-bold mb-2">Password</label>
+          <input v-model="password" type="password" name="password" id="password" class="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500" required>
         </div>
-        <div class="mb-6">
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline w-full" type="submit">
-            Register
+        <div>
+          <button type="submit" class="w-full py-3 px-4 text-white font-bold bg-blue-500 hover:bg-blue-600 rounded-lg focus:outline-none focus:shadow-outline">
+            Create account
           </button>
         </div>
-        <div v-if="error" class="text-red-500 text-sm italic">{{ error }}</div>
+        <div v-if="error" class="text-red-500 italic">{{ error }}</div>
       </form>
     </div>
   </div>
@@ -38,26 +38,34 @@
           username: '',
           password: '',
           name:'',
-          email:''
+          email:'',
+          error:''
         };
       },
      
       methods: {
         async registerUser() {
-      try {
-        const response = await axios.post('http://localhost:3000/users', {
-          username: this.username,
-          password: this.password,
-          name:this.name,
-          email:this.email
-        });
-        console.log(response.data);
-        this.$router.push('/login');
-      } catch (error) {
-        console.error(error);
-      }
+  try {
+   
+    const response = await axios.post('http://localhost:3000/users', {
+      username: this.username,
+      password: this.password,
+      name: this.name,
+      email: this.email
+    });
+    console.log(response)
+   
+    this.$router.push('/login');
+  } catch (error) {
+
+    if (error.response && error.response.status === 409) {
+      this.error = 'User already exists';
+    } else {
+      console.error(error);
     }
-      },
+  }
+}
+      }
     };
   </script>
   
